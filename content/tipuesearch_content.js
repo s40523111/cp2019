a6ab08ad5924fac05e239e9da9135aa74814c0c5
s40523111 <40523111@gm.nfu.edu.tw>
2019-10-03 00:07:30 +0800

    20191003-1

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://s40523111.github.io/cp2019/ \xa0 \n \n\nsrc="https://dartpad.dev/?id=98715172dc3f22bf796622ff9aae07ff"', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://s40523111.github.io/cp2019/ \xa0 \n\nhttps://dartpad.dev/?id=98715172dc3f22bf796622ff9aae07ff\n\n \n', 'tags': '', 'url': 'About.html'}]};
