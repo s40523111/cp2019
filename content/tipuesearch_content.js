453fe9ff22ebe6a18d3f904dd1d8af51f313116a
s40523111 <40523111@gm.nfu.edu.tw>
2019-10-03 15:58:16 +0800

    20191003-2

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://s40523111.github.io/cp2019/ \xa0 \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://s40523111.github.io/cp2019/ \xa0 \n \n\n1', 'tags': '', 'url': 'About.html'}]};
